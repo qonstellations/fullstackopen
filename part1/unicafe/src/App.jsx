8f0b79c4372a03abb23e3617580a82758f436022
qonstellations <aarush.a.pathak@gmail.com>
2026-08-10 21:59:00 +0530

    completed unicafe part-1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const changeGood = () => setGood(good => good+1)
+  const changeNeutral = () => setNeutral(neutral => neutral+1)
+  const changeBad = () => setBad(bad => bad+1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={changeGood} text='good'></Button>
+      <Button onClick={changeNeutral} text='neutral'></Button>
+      <Button onClick={changeBad} text='bad'></Button>
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
