7e3f5ae5583b2660c57d941916f034db627c2844
qonstellations <aarush.a.pathak@gmail.com>
2026-08-11 21:22:29 +0530

    completed part 2.1-2.5

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,43 @@
+const Header = ({ name }) => {
+  return (
+    <h2>{name}</h2>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => <Part key={part.id} part={part} />)}
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((acc, num) => acc+num.exercises, 0)
+
+  return (
+    <p>
+      <strong>total of {total} exercises</strong>
+    </p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
