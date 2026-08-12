19b161aeb81879aeabc99048a9c6198bb6291a9f
qonstellations <aarush.a.pathak@gmail.com>
2026-08-12 21:37:50 +0530

    completed exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newPerson, setNewPerson] = useState({ name:'', number:'', id:undefined })
   const [searchQuery, setSearchQuery] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
   const addPerson = (event) => {
     event.preventDefault()
-
     const isFound = persons.some(
       person => person.name.trim().toLowerCase() === newPerson.name.toLowerCase()
     )
@@ -32,21 +35,12 @@ const App = () => {
       }
       setPersons([...persons, personObject])
     }
-
     setNewPerson({ name: '', number: '', id: undefined })
   }
 
-  const handleNameChange = (event) => {
-    setNewPerson({...newPerson, name: event.target.value })
-  }
-
-  const handleNumberChange = (event) => {
-    setNewPerson({...newPerson, number: event.target.value })
-  }
-
-  const handleSearchQuery = (event) => {
-    setSearchQuery(event.target.value)
-  }
+  const handleNameChange = event => setNewPerson({...newPerson, name: event.target.value })
+  const handleNumberChange = event => setNewPerson({...newPerson, number: event.target.value })
+  const handleSearchQuery = event => setSearchQuery(event.target.value)
 
   return (
     <div>
